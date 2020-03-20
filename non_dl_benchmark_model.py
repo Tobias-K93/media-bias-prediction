06904673ae8d77519a92d0b893aec4550ac1c5a6
Tobias-K93 <tobias.krebs@posteo.de>
2020-03-20 22:58:29 +0100

    setting up hyperparameters tuning in non dl benchmark model

--- non_dl_benchmark_model.py
+++ non_dl_benchmark_model.py
@@ -3,6 +3,7 @@ import time
 import multiprocessing
 import numpy as np
 import torch
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score, accuracy_score
 from sklearn.model_selection import ParameterGrid
@@ -18,13 +19,18 @@ y = torch.load('bias_tensor.pt').numpy()
 character_variables = np.load('non_dl_character_variables.npy')
 dict_count_variables = np.load('non_dl_dict_count_variables.npy')
 token_based_variables = np.load('non_dl_token_based_variables.npy')
+pos_variables = np.load('non_dl_pos_variables.npy')
 
-X = np.concatenate((character_variables,dict_count_variables,token_based_variables),axis=1)
+X = np.concatenate((character_variables,dict_count_variables,
+                    token_based_variables, pos_variables),axis=1)
+
+
+
 
 
 ### creating id vectors
 np.random.seed(123)
-data_length = len(y) #len(tensor_cut_sun_list[0])
+data_length = len(y) 
 
 ids = np.arange(data_length)
 np.random.shuffle(ids) 
@@ -46,14 +52,14 @@ y_val = y[val_ids]
 y_test = y[test_ids]
 
 ### Setting up and training random forest
-cpus = multiprocessing.cpu_count()
+cpus = 5 #multiprocessing.cpu_count()
 
 ### hyper parameter search
 
-parameters = {'n_estimators': [100,200,300,500],
+parameters = {'n_estimators': [200,500],
               'criterion': ['gini', 'entropy'],
-              'max_depth': [None, 5,7,10,14],
-              'min_samples_split': [10,30,50,100,200] ,
+              'max_depth': [None,20,30,40,50],
+              'min_samples_split': [10,20,30,50,100] ,
               'max_features': ['sqrt', None]}
 
 param_grid = ParameterGrid(parameters)
@@ -62,20 +68,56 @@ grid_search_iterations = 10
 
 random_grid_ids = np.random.randint(0,len(param_grid)-1,grid_search_iterations)
 
-for id in random_grid_ids:
-    
+random_grid_scores = []
+for i,id in enumerate(random_grid_ids):
+    random_params = param_grid[id]
 
-    rf = RandomForestClassifier(n_estimators=,
-                                min_samples_leaf=100,
+    rf = RandomForestClassifier(n_estimators=random_params['n_estimators'],
+                                criterion= random_params['criterion'],
+                                min_samples_split=random_params['min_samples_split'],
+                                max_features= random_params['max_features'],
+                                max_depth= random_params['max_depth'],
                                 n_jobs=max(1,cpus-1), 
-                                verbose=1)
+                                verbose=0)
+
+    print(pd.DataFrame(random_params, index=[i+1]))
+    print('------------------------------'*2)
+    start = time.time()
+    rf.fit(X_train, y_train)
+    end = time.time()-start
+    minutes = end // 60
+    seconds = end % 60
+    print('Time: %i minutes %i seconds'%(minutes, seconds))
+
+    # predict on train 
+    train_predictions =  rf.predict(X_train)
+    train_acc = np.sum(y_train==train_predictions)/len(y_train)
+    train_f1 = f1_score(y_train, train_predictions, average='macro')
+
+    # predict on validation
+    val_predictions = rf.predict(X_val)
+    val_acc = np.sum(y_val==val_predictions)/len(y_val)
+    val_f1 = f1_score(y_val, val_predictions, average='macro')
+
+    print('Train:      Accuracy %.4f | F1 %.4f \nValidation: Accuracy %.4f | F1 %.4f'%
+    (train_acc,train_f1,val_acc,val_f1))
+    print('------------------------------'*2)
+    print('------------------------------'*2)
+    
+    random_params['train_acc'] = round(train_acc,4)
+    random_params['train_f1'] = round(train_f1, 4)
+    random_params['val_acc'] = round(val_acc, 4)
+    random_params['val_f1'] = round(val_f1, 4)
+    
+    random_grid_scores.append(random_params)
+
+
+grid_search_results = pd.DataFrame(random_grid_scores)
+grid_search_results
 
 
 
-
-
-
-
+#####################################################################################
 
 rf = RandomForestClassifier(200,
                             min_samples_leaf=100,
@@ -84,7 +126,7 @@ rf = RandomForestClassifier(200,
 
 start= time.time()
 rf.fit(X_train, y_train)
-print('RandomnForest took %f.2 seconds'%(time.time()-start))
+print('RandomnForest took %.2f seconds'%(time.time()-start))
 
 train_predictions =  rf.predict(X_train)
 train_acc = np.sum(y_train==train_predictions)/len(y_train)
@@ -96,7 +138,7 @@ val_predictions = rf.predict(X_val)
 val_acc = np.sum(y_val==val_predictions)/len(y_val)
 val_f1 = f1_score(y_val, val_predictions, average='macro')
 
-print('Train: Accuracy %f.4 | F1 %f.4 \nValidation: Accuracy %f.4 | F1 %f.4'%
+print('Train: Accuracy %.4f | F1 %.4f \nValidation: Accuracy %.4f | F1 %.4f'%
 (train_acc,train_f1,val_acc,val_f1))
 
 
